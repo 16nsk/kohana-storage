01c659795d11fc81f516e190fa66fa2a0062ada3
Gabriel Ignisca <gabriel@designjunkie.com>
2016-03-10 16:22:33 +0000

    php 5.2 does not support that T_STATIC

--- classes/kohana/storage.php
+++ classes/kohana/storage.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
  * Storage Module
- * 
+ *
  * @package		Storage
  * @category	Base
  * @author		Micheal Morgan <micheal@morgan.ly>
@@ -12,14 +12,14 @@ class Kohana_Storage
 {
 	/**
 	 * Directory delimiter
-	 * 
+	 *
 	 * @var		string
 	 */
-	const DELIMITER = '/';	
+	const DELIMITER = '/';
 
 	/**
 	 * Namespace helper for `Storage_Connection` factory pattern.
-	 * 
+	 *
 	 * @static
 	 * @access	public
 	 * @param	mixed	string|NULL
@@ -33,19 +33,19 @@ class Kohana_Storage
 
 	/**
 	 * Trim path
-	 * 
+	 *
 	 * @access	public
 	 * @param	string
 	 * @return	mixed	string|NULL
 	 */
 	public static function trim($path)
 	{
-		return trim(trim($path), static::DELIMITER);
+		return trim($path);
 	}
-	
+
 	/**
 	 * Convert path to hash structure while preserving directory and extension.
-	 * 
+	 *
 	 * @static
 	 * @access	public
 	 * @param	string
@@ -55,26 +55,26 @@ class Kohana_Storage
 	public static function hash($path, $random = FALSE)
 	{
 		$hash = ($random) ? md5(time() + rand()) : md5($path);
-		
+
 		$path = pathinfo($path);
 
 		$segments = array();
-		
+
 		if ($path['dirname'] != '.')
 		{
 			$segments[] = $path['dirname'];
 		}
-	
+
 		$segments[] = substr($hash, 0, 2);
 		$segments[] = substr($hash, 2, 2);
 		$segments[]	= $hash . ((isset($path['extension'])) ? '.' . $path['extension'] : '');
-		
+
 		return implode('/', $segments);
 	}
-	
+
 	/**
 	 * Wrapper for File::mime_by_ext
-	 * 
+	 *
 	 * @static
 	 * @access	public
 	 * @param	resource
@@ -84,10 +84,10 @@ class Kohana_Storage
 	public static function mime($path, $default = 'application/octet-stream')
 	{
 		$extension = strtolower(pathinfo($path, PATHINFO_EXTENSION));
-		
+
 		if ($mime = File::mime_by_ext($extension))
 			return $mime;
-				
+
 		return $default;
 	}
 }
